a460b4fd48a332b2110fa41e460fe0030f1548d1
zabed <zabedalam777@gmail.com>
2020-03-30 10:10:23 +0200

    user model updated

--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,20 @@
-const mongoose=require("mongoose")
-const Schema=mongoose.Schema
-const userSchema=new Schema({
-    username:{
-        type:String,
-        required:true
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userSchema = new Schema(
+  {
+    username: String,
+    name: String,
+    facebookId: String,
+    picture: {
+      type: String,
+      default: "https://via.placeholder.com/300"
     },
-    name:{
-        type:String,
-        required:true
-    },
-    picture:{
-        type:String,
-        default:"https://via.placeholder.com/300"
-    },
-    aboutme:{
-        type:String
-    }
-})
+    aboutme: String,
+    following: [{ type: Schema.Types.ObjectId }],
+    followers: [{ type: Schema.Types.ObjectId }],
+    events: [{ type: Schema.Types.ObjectId, ref: "Event" }]
+  },
+  { timestamps: true }
+);
 
-module.exports=mongoose.model("User",userSchema)
+module.exports = mongoose.model("User", userSchema);
